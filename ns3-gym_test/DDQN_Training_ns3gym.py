42c691cd83b4dd72fa587a61d79ce225f4cd3b11
Tee Yu Chen <yctee1221@gmail.com>
2025-03-04 09:36:38 +0800

    new params

--- ns3-gym_test/DDQN_Training_ns3gym.py
+++ ns3-gym_test/DDQN_Training_ns3gym.py
@@ -21,11 +21,11 @@ class SingleDDQNAgent:
         self.cio_options = [-10, 0, 10]
         
         # Hyperparameters
-        self.memory = deque(maxlen=50000) 
+        self.memory = deque(maxlen=30000) 
         self.gamma = 0.95    
         self.epsilon = 1.0   
         self.epsilon_min = 0.05
-        self.epsilon_decay = 0.9998 # 0.9995 use (min_epsilon/initial_epsilon)^(1/decay_steps) to find the decay rate, better to drop to minimum at 75% of training step
+        self.epsilon_decay = 0.999966714 # 0.9995 use (min_epsilon/initial_epsilon)^(1/decay_steps) to find the decay rate, better to drop to minimum at 75% of training step
         self.learning_rate = 0.001 # 0.001
         self.update_target_frequency = 1000 #500
         self.batch_size = 64
@@ -199,7 +199,7 @@ def main():
         num_enbs = 3
         agent = IndependentDDQNAgent(state_size, num_enbs)
         
-        n_episodes = 200
+        n_episodes = 900
         max_steps = 100
         reward_history = []
         epsilon_history = []
